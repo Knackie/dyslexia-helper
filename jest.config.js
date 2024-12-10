cb3e7f45a861813f4a780106e2dad58e809af112
Knackie <43431712+Knackie@users.noreply.github.com>
2024-12-10 15:14:06 +0100

    fixjsdom error

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
-
 module.exports = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"], // Load setup file for mocks
-  testEnvironment: "jsdom", // Simulate browser environment
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
 };
